ef42cbe6f11761a4f1ece9da7073953cb265f143
anthonyBgood <anthonyBpostbox@gmail.com>
2019-05-22 10:47:04 +1000

    reducers - enabled initial data

---
+++ src/reducers/authedUser.js
@@ -0,0 +1,12 @@
+import AUTHED_USER from '../actions/authedUser'
+
+
+export default function users (state = null, action){
+  switch(action.type){
+    case AUTHED_USER :
+      return action.id
+      
+    default :
+      return state
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux'
+import tweets from './tweets'
+import users from './users'
+import authedUser from './authedUser'
+
+
+
+export default function combineReducers({
+  authedUser,
+  users,
+  tweets,
+})
+
